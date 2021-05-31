a3562de7e24bf584aa72cd779cb9ebd13f41a8a3
Avishkar Patil <avishkarpatil29@gmail.com>
2021-05-31 18:30:30 +0530

    Initial Commit

--- m3u8.php
+++ m3u8.php
@@ -6,11 +6,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Expose-Headers: Content-Length,Content-Range");
 header("Access-Control-Allow-Headers: Range");
 header("Accept-Ranges: bytes");
-#use token file - it will generate tokens on every request and is useful if you want to make streams public . Tokens will not be banned as they are uniqe for each user.
-$p= @file_get_contents("http://localhost/token.php");
-# This is single token generated based on TIME 
-# if more than 10 users will watch stream ar same time i.e 10:10:59 so 10 users will have same token. 
 
+$p= @file_get_contents("http://localhost/token.php");
 
 
 $_SESSION["p"]=$p;
@@ -23,17 +20,11 @@ $opts = [
         "method" => "GET",
         "header" => "User-Agent: plaYtv/5.3.2 (Linux;Android 5.1.1) ExoPlayerLib/2.3.0\r\n" 
 
-
     ]
 ];
 
 $cx = stream_context_create($opts);
 
-# Useful Links - Channels shuffer across all servers , most 99% channels works on all servers. 
-    #http://mumsite.cdnsrv.jio.com/jiotv.live.cdn.jio.com/
-    #http://hdbdsite.cdnsrv.jio.com/jiotv.live.cdn.jio.com/
-    #http://gdcsite.cdnsrv.jio.com/hotstar.live.cdn.jio.com/
-    #http://gdcsite.cdnsrv.jio.com/jiotv.live.cdn.jio.com/
 $hs = file_get_contents("https://jiotvweb.cdn.jio.com/jiotv.live.cdn.jio.com/" . $_REQUEST["c"] . "/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . ".m3u8" .  $p,false,$cx);
 
 $hs= @preg_replace("/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] ."-([^.]+\.)key/", 'stream.php?key='  . $_REQUEST["c"] . '/' .   $_REQUEST["c"] . '_' . $_REQUEST["q"] . '-\1key', $hs);
@@ -44,5 +35,4 @@ $hs = str_replace("https://tv.media.jio.com/streams_hotstar/" . $_REQUEST["c"] .
 print $hs;
 
 }
-
 ?>
